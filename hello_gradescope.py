1c94416c22b3e842f325bf26e7ed00a9ec98059b
maritzahambric <devicenm@oregonstate.edu>
2024-04-08 20:18:13 -0700

    This confirms that gradescope is correctly connected

---
+++ hello_gradescope.py
@@ -0,0 +1,9 @@
+# Name:Maritza Devicente Hambric
+# OSU Email:devicenm@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment:Assignment 0: Introduction, Environment Set-up, and Debugging
+# Due Date: Apr 10 at 11:59pm
+# Description:This confirms that gradescope is correctly connected
+
+def gradescope_test():
+    return 42
